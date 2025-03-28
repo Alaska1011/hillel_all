e3bc17c0ecaac934c64b7a41ca89ed4bf34f73d6
igord <igord1707@gmail.com>
2025-03-28 22:45:34 +0200

    commit

--- lesson2/lesson.py
+++ lesson2/lesson.py
@@ -1,8 +1,21 @@
-a = int(input('enter n number: '))
-n = 0
-while a>0:
-    z=a%10
-    a=a//10
-    n=n*10
-    n=n+z
-print(n)
+
+number = int(input("Enter 4-digit number: "))
+
+if 0<= number <=9999:
+    thousands = number // 1000
+
+    hundreds = (number // 100) % 10
+
+    tens = (number // 10) % 10
+
+    units = number % 10
+
+    print(thousands)
+
+    print(hundreds)
+
+    print(tens)
+
+    print(units)
+else:
+    print("давай по новой")
