cc9c7be2eeb0b5c56badd03f5c80360f247ba15a
igord <igord1707@gmail.com>
2025-04-04 20:47:37 +0300

    commit

---
+++ lesson3/lesson.py
@@ -0,0 +1,9 @@
+def absolut(lst):
+    if len(lst) <= 1:
+        return lst
+    last_element = lst[-1]
+    return [last_element] + lst[:-1]
+print(absolut([12,3,4,10,]))
+print(absolut([1]))
+print(absolut([]))
+print(absolut([12, 3, 4, 10, 8]))
