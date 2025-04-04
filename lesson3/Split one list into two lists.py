f646a7b4b431dc71a2f5821ba2a58d5bd81b189d
igord <igord1707@gmail.com>
2025-04-04 21:47:51 +0300

    commit

--- lesson3/Split one list into two lists.py
+++ lesson3/Split one list into two lists.py
@@ -3,18 +3,11 @@ def absolut2(lst):
         return [[], []]
 
     mid_index = (len(lst) + 1) // 2
-    first_list = []
-    second_list = []
-
-    for i in range(len(lst)):
-        if i < mid_index:
-            first_list.append(lst[i])
-        else:
-            second_list.append(lst[i])
-
-        return [first_list, second_list]
+    first_list = lst[:mid_index]
+    second_list = lst[mid_index:]
+    return [first_list, second_list]
 print(absolut2([1, 2, 3, 4, 5, 6]))
 print(absolut2([1,2,3]))
 print(absolut2([1,2,3,4,5]))
 print(absolut2([1]))
-print(absolut2([0]))
+print(absolut2([]))
