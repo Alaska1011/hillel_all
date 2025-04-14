24362457a795cdf196947ca7677c9e1a28eee1f2
igord <igord1707@gmail.com>
2025-04-14 23:05:13 +0300

    commit

--- lesson2/lesson2dz.py
+++ lesson2/lesson2dz.py
@@ -2,3 +2,4 @@ number = int(input('Enter 5 number'))
 reversed_number = ((number % 10) * 10000) + ((number // 10 % 10) * 1000) + ((number // 100 % 10) * 100) + ((number // 1000 % 10) * 10) + (number // 10000)
 print(f": {reversed_number}")
 
+1
--- lesson3/lesson.py
+++ lesson3/lesson.py
@@ -1,9 +1,13 @@
-def absolut(lst):
-    if len(lst) <= 1:
-        return lst
-    last_element = lst[-1]
-    return [last_element] + lst[:-1]
-print(absolut([12,3,4,10,]))
-print(absolut([1]))
-print(absolut([]))
-print(absolut([12, 3, 4, 10, 8]))
+list_of_lists = [
+    [12, 3, 4, 10],
+    [1],
+    [],
+    [12, 3, 4, 10, 8]
+]
+for my_list in list_of_lists:
+    if len(my_list) > 1:
+        last_element = my_list[-1]
+        new_list = [last_element] + my_list[:-1]
+    else:
+        new_list = my_list
+    print(new_list)
