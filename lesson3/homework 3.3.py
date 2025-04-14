f4f704eebfa95ae1b8fd5b19e8135932eeb94370
igord <igord1707@gmail.com>
2025-04-14 23:27:40 +0300

    commit

--- lesson3/homework 3.3.py
+++ lesson3/homework 3.3.py
@@ -1,24 +1,26 @@
-while True:
-    print("Enter firs number: ")
-    num1 = float(input())
-    print("Enter operation (+, -, *, /): ")
-    operation = input()
-    print("Enter second number: ")
-    num2 = float(input())
-    if operation == '+':
-        result = num1 + num2
-        print("result:", result)
-    elif operation == '-':
-        result = num1 - num2
-        print("result:", result)
-    elif operation == '*':
-        result = num1 * num2
-        print("result:", result)
-    elif operation == '/':
-        if num2 == 0:
-            print("Error: divisor cannot be 0-supplemented!")
-        else:
-            result = num1 / num2
-            print("result:", result)
+print("Enter first number: ")
+num1 = float(input())
+
+print("Enter operation (+, -, *, /): ")
+operation = input()
+
+print("Enter second number: ")
+num2 = float(input())
+
+if operation == '+':
+    result = num1 + num2
+    print("result:", result)
+elif operation == '-':
+    result = num1 - num2
+    print("result:", result)
+elif operation == '*':
+    result = num1 * num2
+    print("result:", result)
+elif operation == '/':
+    if num2 == 0:
+        print("Error Divisor cannot be equal to 0")
     else:
-        print("Error: Invalid operation! Try again.")
+        result = num1 / num2
+        print("result:", result)
+else:
+    print("Error Invalid operation! Try again")
